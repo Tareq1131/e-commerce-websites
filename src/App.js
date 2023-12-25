31bd2e093ac3fd71845e9404234f34af617dde53
Tareq1131 <tareqislam076@gmail.com>
2023-12-26 01:30:05 +0600

    Passing Functionality to each component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Recommended from "./Recommended/Recommended";
 import Sidebar from "./Sidebar/Sidebar";
 
 import products from "./db/data";
+import Card from "./components/Card";
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState(null);
@@ -31,6 +32,42 @@ function App() {
     setSelectedCategory(event.target.value);
   };
 
+  function filteredData(products,selected, query){
+    let filteredProducts = products; 
+
+       // Filtering Input Items
+       if (query) {
+        filteredProducts = filteredItems;
+      }
+
+       // Applying selected filter
+       if (selected) {
+        filteredProducts = filteredProducts.filter(
+          ({ category, color, company, newPrice, title }) =>
+            category === selected ||
+            color === selected ||
+            company === selected ||
+            newPrice === selected ||
+            title === selected
+        );
+      }
+
+      return filteredProducts.map(
+        ({ img, title, star, reviews, prevPrice, newPrice }) => (
+          <Card
+            key={Math.random()}
+            img={img}
+            title={title}
+            star={star}
+            reviews={reviews}
+            prevPrice={prevPrice}
+            newPrice={newPrice}
+          />
+        )
+      );
+
+  }
+  const result = filteredData(products, selectedCategory, query);
   return (
     <>
       <Sidebar />
