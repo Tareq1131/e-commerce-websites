a3334157c6deb7881cece61a9d2a7b3798bc7319
Tareq1131 <tareqislam076@gmail.com>
2023-12-26 02:02:02 +0600

    Refactor input Category price color Card Components

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -4,16 +4,16 @@ import Category from "./Category/Category";
 import Price from "./Price/Price";
 import Color from "./Color/Color";
 
-function Sidebar() {
+function Sidebar({handleChange}) {
   return (
     <>
       <section className="sidebar">
         <div className="logo-container">
           <h1>🛒</h1>
         </div>
-        <Category />
-        <Price />
-        <Color />
+        <Category handleChange={handleChange}/>
+        <Price handleChange={handleChange}/>
+        <Color handleChange={handleChange}/>
       </section>
     </>
   );
