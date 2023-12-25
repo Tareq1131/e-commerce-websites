2188b3d04e58a8bd6aabc3b9a1326abb1aea2121
Tareq1131 <tareqislam076@gmail.com>
2023-12-25 23:42:48 +0600

    Created recommended section

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
   <>
     <Navigation />
-    <Product/>
     <Recommended/>
+    <Product/>
   </>
    
    
--- src/Recommended/Recommended.js
+++ src/Recommended/Recommended.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import './Recommended.css'
+import Button from '../components/Button'
 
 function Recommended() {
   return (
+    <>
     <div>
-      recomm
+      <h2 className="recommended-title">Recommended</h2>
+      <div className="recommended-flex">
+        <button className='btns'>All Products</button>
+        <button className='btns'>Nike</button>
+        <button className='btns'>Adidas</button>
+        <button className='btns'>Puma</button>
+        <button className='btns'>Vans</button>
+       
+      </div>
     </div>
+  </>
   )
 }
 
